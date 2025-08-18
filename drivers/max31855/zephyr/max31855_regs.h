99a2472b13db3b9ff2cd476e171632fca5d8df38
nafihahmd <nafihahamd@gmail.com>
2025-08-18 11:09:31 +0530

    Add MAX31855 custom driver

---
+++ drivers/max31855/zephyr/max31855_regs.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef __MAX31855_REGS_H__
+#define __MAX31855_REGS_H__
+
+#define THERMOCOUPLE_TEMPERATURE_POS 18
+#define INTERNAL_TEMPERATURE_POS     4
+#define THERMOCOUPLE_SIGN_BITS	     0xffff2000
+#define INTERNAL_SIGN_BITS	     0xfffff800
+#define THERMOCOUPLE_RESOLUTION	     25
+#define INTERNAL_RESOLUTION	     625
+
+#endif /* __MAX31855_REGS_H__ */
